b062495e7556cdb662f71f7e64a024a2dfc1c5ff
Sergey Odintsov <mobilotest@gmail.com>
2023-02-14 13:06:30 -0800

    recursion 2

--- src/Recurrsion.java
+++ src/Recurrsion.java
@@ -1,11 +1,32 @@
 public class Recurrsion {
-            public static void main(String[] args) {
-                countDown(3);
-            }
-            public static void countDown (int n) {
-                System.out.println(n);
-                if(n > 1) {
-                    countDown(n-1);
-                }
-            }
+    public static void main(String[] args) {
+//        countDown(3);
+        sumOf(3);
+    }
+    public static void countDown (int n) {
+        System.out.println(n);
+        if(n > 1) {
+            countDown(n-1);
         }
+    }
+
+    public static int sumOf(int n) {
+        int sum;
+        if(n == 1) {
+            sum = 1;
+        }else{
+            sum = sumOf(n - 1) + n;
+        }
+        return sum;
+    }
+
+//    public void displayBackward() {
+//        displayChainBackward(topNode);
+//    }
+//    private void displayChainBackward(Node node) {
+//        if(node == null)
+//            return;
+//        displayChainBackward(node.getNextNode());
+//        System.out.println(node.getData());
+//    }
+}
