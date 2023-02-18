ce03e750596f59f998ec2a355ed01bf20b8e735c
Sergey Odintsov <mobilotest@gmail.com>
2023-02-17 19:20:13 -0800

    changed Assestment 2_1 to a new from scratch

--- src/Assestment_2_1/Lab2.java
+++ src/Assestment_2_1/Lab2.java
@@ -2,25 +2,20 @@ package Assestment_2_1;
 
 class Lab2 {
 	public static void main(String[] arg) {
-	
 	}
-	// Problem 1
+	// Problem 4
 	public static int min(int [] a, int begin, int end) {
-
+		return 0;
 	}
-	
-	// Problem 2
+	// Problem 5
 	public static long computePay(int day) {
-	
+		return 0;
 	}
-	
 	public static long computeSavings(int day) {
-	
+		return 0;
 	}
-	
-	// Problem 3
-	public static int countSubstring(String s1, String s2) {
-	
-	
+	// Problem 6
+	public static int countSubstrings(String s1, String s2) {
+		return 0;
 	}
 }
