5281ce6e83d4ddcd8cf939b254d29d129e67a60d
Sergey Odintsov <mobilotest@gmail.com>
2023-02-22 19:38:37 -0800

    changed Assestment 2_1 done

--- src/Assestment_2_1/Lab2.java
+++ src/Assestment_2_1/Lab2.java
@@ -19,8 +19,8 @@ class Lab2 {
 		System.out.println("- - - - - - - - - - - - - -");
 
 		String s1 = "Java is a programming language originally developed by James Gosling at Sun Microsystems and released in 1995 as a core component of Sun Microsystems Java platform. The language derives much of its syntax from C and C++ but has simpler object model and fewer low-level facilities. Java applications are typically compiled to bytecode (class file) that can run on any Java Virtual Machine (JVM) regardless of computer architecture. Java is a general-purpose, concurrent, class-based, object-oriented language that is specifically designed to have as few implementation dependencies as possible. Java is currently one of the most popular programming languages in use, particularly for client-server web applications, with a reported 10 million users.";
-		String s2= "Java";
-		countSubstrings(String s1, String s2);
+		String s2 = "Java";
+		System.out.println(countSubstrings(s1,s2));
 	}
 	// Problem 4
 	public static int min(int [] a, int begin, int end) {
@@ -64,17 +64,12 @@ class Lab2 {
 
 	// Problem 6
 	public static int countSubstrings(String s1, String s2) {
-		int  s1_length = s1.length();
-		int  s2_length = s2.length();
-		int count = 0;
-		for(int i=0; i<=s1_length; i++){
-			for(int j=0; j<=s2_length; j++){
-				if(s1.indexOf(j).equals(s2.indexOf(i))){
-					count++;
-				}
-			}
+		int index = s1.indexOf(s2);
+		if (index == -1) {
+			return 0;
+		} else {
+			return 1 + countSubstrings(s1.substring(index + s2.length()), s2);
 		}
-		return count;
 	}
 }
 
@@ -84,3 +79,5 @@ class Lab2 {
 //		Enter how may days you plan to work: 39
 //		On the day 39 you will earn 274877906944 cents per day
 //		By the day 39 you will collect 549755813887 cents in savings
+//		- - - - - - - - - - - - - -
+//		6
--- src/Assestment_2_1/RecursionTest.java
+++ src/Assestment_2_1/RecursionTest.java
@@ -1,27 +1,27 @@
 package Assestment_2_1;
 
-public class RecursionTest {
-    public static int computePay(int day) {
-        if (day == 1) {
-            return 1;
-        } else {
-            return 2 * computePay(day - 1);
-        }
-    }
+import java.util.Scanner;
 
-    public static long computeSavings(int day) {
-        if (day == 1) {
-            return 1;
-        } else {
-            return computePay(day) + computeSavings(day - 1);
-        }
-    }
-
+public class RecursionTest extends Lab2{
     public static void main(String[] args) {
-        int day = 39;
-        int pay = computePay(day);
-        long savings = computeSavings(day);
-        System.out.println("Pay on day " + day + ": " + pay + " cents");
-        System.out.println("Savings on day " + day + ": " + savings + " cents");
+        int [] a = { 2, 3, 5, 7, 11, 13, 17, 19, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 23, 29, 31, 37, 41, 43 };
+        int min = min(a, 10,20);
+        System.out.println("The minimum element in the array: " + min);
+
+        System.out.println("- - - - - - - - - - - - - -");
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Enter how may days you plan to work: ");
+        int days = myObj.nextInt();  // Read user input
+        System.out.println("On the day " + days + " you will earn " + computePay(days) + " cents per day");
+        System.out.println("By the day " + days + " you will collect " + computeSavings(days) + " cents in savings");
+
+        System.out.println("- - - - - - - - - - - - - -");
+
+        String s1 = "Recursion is the technique of making a function call itself. This technique provides a way to break complicated problems down into simple problems which are easier to solve.\n" +
+                "\n" +
+                "Recursion may be a bit difficult to understand. The best way to figure out how it works is to experiment with it.";
+        String s2 = "a";
+        System.out.println(countSubstrings(s1,s2));
     }
 }
