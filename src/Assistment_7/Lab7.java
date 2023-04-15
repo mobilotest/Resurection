b6b44874b6650a9e5347b2612f7b49210d3a0511
Sergey Odintsov <mobilotest@gmail.com>
2023-04-15 12:05:43 -0700

    assestment 7 continue 1

--- src/Assistment_7/Lab7.java
+++ src/Assistment_7/Lab7.java
@@ -6,12 +6,28 @@ public class Lab7
 {
 	// Problem 1
 	public static int findMissing(int [] a) {
-
+		int n = a.length;
+		int expectedSum = (n + 1) * (n + 2) / 2;
+		int actualSum = 0;
+		for (int i = 0; i < n; i++) {
+			actualSum += a[i];
+		}
+		return expectedSum - actualSum;
 	}
 	
 	// Problem 2
 	public static void countingSort(int[] a, int n ) {
-
+		int[] count = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			count[a[i] - 1]++;
+		}
+		int index = 0;
+		for (int i = 0; i < n; i++) {
+			while (count[i] > 0) {
+				a[index++] = i + 1;
+				count[i]--;
+			}
+		}
 	} 
 
 	// Problem 3
