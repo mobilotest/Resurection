7b3886939a4a04e177617877b9671ab5e1bf1606
Sergey Odintsov <mobilotest@gmail.com>
2023-04-15 09:22:31 -0700

    assestment 7 beginning

---
+++ src/Assistment_7/Lab7.java
@@ -0,0 +1,25 @@
+package Assistment_7;
+
+import java.util.* ;
+
+public class Lab7
+{
+	// Problem 1
+	public static int findMissing(int [] a) {
+
+	}
+	
+	// Problem 2
+	public static void countingSort(int[] a, int n ) {
+
+	} 
+
+	// Problem 3
+	public satic int findKSmallest(int[] a, int k) {
+		
+	}
+
+	public satic int findMedian(int[] a, int k) {
+		
+	}
+} 
